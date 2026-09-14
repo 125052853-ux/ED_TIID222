560d2774b81f2b2f91fdb4c5e97a352f200a1af6
AntonioRmz <125052853@upq.edu.mx>
2026-09-14 17:20:53 -0600

    Se trabajo con arreglos unidimensionales

---
+++ Arreglos.py
@@ -0,0 +1,32 @@
+#Declarando un arreglo
+numeros = [10, 20, 30, 40, 50,]
+
+#Imprimo un elemento del especifico del arreglo
+print(numeros[2])
+
+#Reasignacion 
+numeros[3] = 35
+print(numeros[3])
+
+#Agrega un nuevo valor al final del arreglo
+numeros.append(60)
+print(numeros)
+
+#Eliminamos un numero definido
+numeros.remove(35)
+print(numeros)
+
+#Eliminamos un valor del arreglo usando la posicion
+numeros.pop(4)
+print(numeros)
+
+#
+frutas = ["Manzana", "Fresa", "Sandia", "Mango", "Melon", "Platano"]
+frutas.pop(4)
+print(frutas)
+
+
+frutas.remove("Manzana")
+print(frutas)
+
+
