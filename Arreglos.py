24e44a01d9d26e33a1b8a0167061e4d609cf816f
AntonioRmz <125052853@upq.edu.mx>
2026-09-21 16:37:28 -0600

    Se trabajo con arreglos y se hizo un programa de cincuentizar numeros

--- Arreglos.py
+++ Arreglos.py
@@ -1,4 +1,4 @@
-#Declarando un arreglo
+"""#Declarando un arreglo
 numeros = [10, 20, 30, 40, 50,]
 
 #Imprimo un elemento del especifico del arreglo
@@ -20,13 +20,52 @@ print(numeros)
 numeros.pop(4)
 print(numeros)
 
-#
+#Eliminamos un elemento del arreglo usando el numero
 frutas = ["Manzana", "Fresa", "Sandia", "Mango", "Melon", "Platano"]
 frutas.pop(4)
 print(frutas)
 
 
 frutas.remove("Manzana")
-print(frutas)
+print(frutas)  
+
+
+arreglo = []
+
+print(arreglo)
+
+n = int(input("INGRESE EL TAMANO DEL ARREGLO: "))
+
+arreglo = [0] * n
+
+for i in range(n):
+    dato = int(input("INGRESE UN NUMERO: "))
+    arreglo[i] = dato
+ """
+
+
+numeros = []
+
+for i in range(15):
+    while True:
+        num = int(input(f"INGRESE UNOS VALORES {i + 1} (0-500): "))
+        if 0 <= num <= 500:
+            numeros.append(num)
+            break
+        else:
+            print("INGRESE UN NUMERO ENTRE 1 AL 500")
+
+print(numeros)
+
+cincuentizar = []
+
+for num in numeros:
+    if num % 5 == 0:
+        cincuentizar.append(num)
+    else:
+        sig = num + (5 - num % 5)
+        cincuentizar.append(sig)
+
+print(cincuentizar)
 
 
